4fd76cf27303c9512afbb030418412f8eca74d49
Adam Jeffries <adamljeffries@gmail.com>
2022-10-30 17:43:55 -0400

    Decimal.places added master loop

--- Program.cs
+++ Program.cs
@@ -6,29 +6,51 @@
         {
            
             {
-
-                Console.Write("What is your hourly rate?");
-                int v1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("How many Regular Time hours did you work?");
-                int v2 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("How many Overtime hours did you work this week?");
-                int v3 = Convert.ToInt32(Console.ReadLine());
-                int grossPay = (v1 * v2);
-                 
-                int overtimePay = (v1 * v3 / 2) + v1 * v3;
-                int OT = overtimePay + grossPay;
-
-                switch (v3)
+                bool stayOpen = true;
+                
+                do
                 {
-                    case >= 0:
-                        Console.WriteLine($"Your gross pay and overtime will be in the amount of ${OT}");
+                    Console.WriteLine("What is your hourly rate?");
 
-                        break;
-                    default:
-                        Console.WriteLine($"Your gross pay is + {grossPay} ");
-                        break;
-                }
+                    //TODO: Fix for case where user would enter "Twenty dollars an hour"
+                    double hourlyRate = Convert.ToDouble(Console.ReadLine().Replace("$", ""));
+                    Console.WriteLine("How many Regular Time hours did you work?");
+                    double regularTime = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("How many Overtime hours did you work his week?");
+                    double overTime = Convert.ToDouble(Console.ReadLine());
+                    double grossPay = (hourlyRate * regularTime);
 
+                    double overtimePay = (hourlyRate * overTime / 2) + hourlyRate * overTime;
+                    double OT = overtimePay + grossPay;
+
+                    switch (overTime)
+                    {
+                        case >= 0:
+                            Console.WriteLine($"Your gross pay and overtime will be in the amount of ${OT}");
+                            break;
+                        default:
+                            Console.WriteLine($"Your gross pay is + {grossPay}");
+                            break;
+                    }
+
+                    Console.WriteLine("\r\nWould you like to calculate another payroll?");
+                    string answer = Console.ReadLine().ToLower();
+
+                    if (answer != "yes") // TODO: Better user answer acceptance
+                    {
+                        stayOpen = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\r\n========================================");
+                        Console.WriteLine("========Dolla Dolla Bills, Yall!=========");
+                        Console.WriteLine("========================================\r\n");
+                    }
+                } while (stayOpen);
+
+                Console.WriteLine("\r\n====================================================");
+                Console.WriteLine("=========Don't spend it all in one place!===========");
+                Console.WriteLine("====================================================");
             }
         }
     }
